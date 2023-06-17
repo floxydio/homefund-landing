968c0c2928d2635802347f60b5f97cb60a851c86
floxydio <diooktar@gmail.com>
2023-06-17 11:59:28 +0700

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'HomeFund - Langkah kecil di investasi.';
+export const SITE_DESCRIPTION = 'Homefund your application';

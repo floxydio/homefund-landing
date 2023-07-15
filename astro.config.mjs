5d15e54ea754a8dbdb86f3addf437b01030b4528
floxydio <diooktar@gmail.com>
2023-07-15 19:29:22 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), vue()]
+  integrations: [mdx(), sitemap(), react(), vue(), robotsTxt()]
 });
